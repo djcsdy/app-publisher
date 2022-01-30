1e687b58cc889f0d9797ffcf909bb38cbca3948c
Scott Meesseman <spmeesseman@gmail.com>
2022-01-30 03:01:07 -0500

    style(eslint): adjust exclude path in config

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
         "sourceType": "module",
         "ecmaVersion": 2019
     },
+    "ignorePatterns": ["**/*.js", "*.json" ],
     "plugins": [
         "eslint-plugin-jsdoc",
         "eslint-plugin-no-null",
