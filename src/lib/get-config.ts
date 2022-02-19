2894d7f536ff612a971ce81d53bccc56e23a1b36
Scott Meesseman <spmeesseman@gmail.com>
2022-02-19 12:21:05 -0500

    fix(general): if ran outside of a project directory containing a .publishrc configuration file, an unhandled exception occurrs.

--- src/lib/get-config.ts
+++ src/lib/get-config.ts
@@ -3,12 +3,9 @@ import { castArray, pickBy, isNil, isString, isPlainObject } from "lodash";
 import { IContext, IOptions } from "../interface";
 import { readFile } from "./utils/fs";
 import { getNpmFile } from "./version/npm";
-import { validatePlugin, parseConfig } from "./plugins/utils";
 import cosmiconfig from "cosmiconfig";
 import resolveFrom from "resolve-from";
 import envCi from "@spmeesseman/env-ci";
-// const plugins = require("./plugins");
-const PLUGINS_DEFINITIONS = require("./definitions/plugins");
 
 export = getConfig;
 
@@ -53,59 +50,27 @@ async function getConfig(context: IContext, opts: IOptions)
 
     options.configFilePath = filepath;
 
-    const pluginsPath = { path: undefined };
-    let extendPaths;
+    let extendPath: string;
     // eslint-disable-next-line prefer-const
-    ({ extends: extendPaths, ...options } = options);
+    ({ extends: extendPath, ...options } = options);
 
-    if (extendPaths)
+    if (extendPath)
     {   //
-        // If `extends` is defined, load and merge each shareable config with `options`
+        // If `extends` is defined, load and merge shareable config with `options`
         //
-        options = {
-            ...castArray(extendPaths).reduce((result, extendPath) =>
-            {
-                const extendsOpts = require(resolveFrom.silent(__dirname, extendPath) || resolveFrom(cwd, extendPath));
-                //
-                // For each plugin defined in a shareable config, save in `pluginsPath` the extendable
-                // config path, so those plugin will be loaded relatively to the config file
-                //
-                Object.entries(extendsOpts)
-                .filter(([, value]) => Boolean(value))
-                .reduce((pluginsPath, [option, value]) =>
-                {
-                    castArray(value).forEach(plugin =>
-                    {
-                        if (option === "plugins" && validatePlugin(plugin))
-                        {
-                            pluginsPath[parseConfig(plugin)[0]] = extendPath;
-                        }
-                        // eslint-disable-next-line dot-notation
-                        else if (PLUGINS_DEFINITIONS[option] && (isString(plugin) || (isPlainObject(plugin) && isString(plugin["path"]))))
-                        {
-                            // eslint-disable-next-line dot-notation
-                            pluginsPath[isString(plugin) ? plugin : plugin["path"]] = extendPath;
-                        }
-                    });
-                    return pluginsPath;
-                }, pluginsPath);
-
-                return { ...result, ...extendsOpts };
-            }, {}),
-            ...options,
-        };
+        const extendsOpts = require(resolveFrom.silent(__dirname, extendPath) || resolveFrom(cwd, extendPath));
+        options =  { ...extendsOpts, ...options };
     }
 
     //
     // Set default options values if not defined yet
     //
     options = {
-        branch: (await defBranch(context)),
-        repo: (await pkgRepoUrl(context)), // || (await repoUrl(context)),
-        repoType: (await pkgRepoType(context)),
+        branch: options.configFilePath ? (await defBranch(context)) : undefined,
+        repo: options.configFilePath ? (await pkgRepoUrl(context)) : undefined, // || (await repoUrl(context)),
+        repoType: options.configFilePath ? (await pkgRepoType(context)) : undefined,
         // eslint-disable-next-line no-template-curly-in-string
         tagFormat: "v${version}",
-        plugins: [],
         // Remove `null` and `undefined` options so they can be replaced with default ones
         ...pickBy(options, option => !isNil(option)),
     };
@@ -120,18 +85,17 @@ async function getConfig(context: IContext, opts: IOptions)
     let optStr = JSON.stringify(options);
     for (const key in process.env)
     {
-        const envVar = "[$][{]\\b" + key + "\\b[}]";
-        optStr = optStr.replace(new RegExp(envVar, "gmi"), process.env[key].replace(/\\/g, "\\\\"));
+        if ({}.hasOwnProperty.call(process.env, key))
+        {
+            const envVar = "[$][{]\\b" + key + "\\b[}]";
+            optStr = optStr.replace(new RegExp(envVar, "gmi"), process.env[key].replace(/\\/g, "\\\\"));
+        }
     }
     options = JSON.parse(optStr);
 
     options.ciInfo = envCi({ env, cwd, repoType: options.repoType });
 
-    //
-    // TODO - plugins maybe?
-    //
-    return { options, plugins: [] };
-    // return { options, plugins: await plugins({ ...context, options }, pluginsPath) };
+    return options;
 }
 
 
